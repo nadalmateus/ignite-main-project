e22528885669edc35b68b7e015b1a78a87421414
Mateus Nadal <nadalmateus@outlook.com>
2021-05-24 19:27:10 -0300

    feat(repository): validating the category registration

--- source/route/categories.route.ts
+++ source/route/categories.route.ts
@@ -7,6 +7,14 @@ const categoryRepository = new CategoryRepository()
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body
 
+  const categoryAlreadyExists = categoryRepository.findByName(name)
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({
+      error: 'Categoy already exists!'
+    })
+  }
+
   categoryRepository.create({ name, description })
 
   return response.status(201).send()
