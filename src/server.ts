b83f6ee177056afced77c2c8a491dfb33036fb0c
Mateus Nadal <nadalmateus@outlook.com>
2021-05-24 18:33:43 -0300

    feat(models): category model

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import { routes } from './routes'
+import { categoriesRoutes } from './route/categories.routes'
 
 const app = express()
-app.use(routes)
+app.use(express.json())
+app.use('/categories', categoriesRoutes)
 
 app.listen(3333, () =>
   console.log('[ON]server running on http://localhost:3333/')
