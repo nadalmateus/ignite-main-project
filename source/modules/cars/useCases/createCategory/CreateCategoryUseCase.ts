a4b09763934bc82802681ca7d4a3d02f0a6435d4
Mateus Nadal <nadalmateus@outlook.com>
2021-05-28 14:57:50 -0300

    refactor(usecase): creating the Category Use Cases

--- source/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ source/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,11 +1,11 @@
-import { CategoryRepository } from '../repositories/CategoryRepository';
+import { CategoryRepository } from '../../repositories/CategoryRepository';
 
 interface IRequest {
 	name: string;
 	description: string;
 }
 
-class CreateCategoryService {
+class CreateCategoryUseCase {
 	constructor(private categoryRepository: CategoryRepository) {}
 
 	execute({ name, description }: IRequest): void {
@@ -19,4 +19,4 @@ class CreateCategoryService {
 	}
 }
 
-export { CreateCategoryService };
+export { CreateCategoryUseCase };
--- source/routes/categoryRoute.ts
+++ source/routes/categoryRoute.ts
@@ -1,19 +1,13 @@
 import { Router } from 'express';
 
 import { CategoryRepository } from '../modules/cars/repositories/CategoryRepository';
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
 
 const categoryRoute = Router();
 const categoryRepository = new CategoryRepository();
 
 categoryRoute.post('/', (request, response) => {
-	const { name, description } = request.body;
-
-	const createCategoryService = new CreateCategoryService(categoryRepository);
-
-	createCategoryService.execute({ name, description });
-
-	return response.status(201).send();
+	return createCategoryController.handle(request, response);
 });
 
 categoryRoute.get('/', (request, response) => {
