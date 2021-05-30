64e952fd7c145b0b76ad853d124a9550a4e24c29
Mateus Nadal <nadalmateus@outlook.com>
2021-05-30 19:20:56 -0300

    ok

--- source/routes/categoryRoute.ts
+++ source/routes/categoryRoute.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/category/createCategory';
 import { listCategoryController } from '../modules/cars/useCases/category/listCategory';
 
 const categoryRoute = Router();
+
+const upload = multer({
+	dest: './tmp',
+});
 
 categoryRoute.post('/', (request, response) => {
 	return createCategoryController.handle(request, response);
@@ -11,6 +16,13 @@ categoryRoute.post('/', (request, response) => {
 
 categoryRoute.get('/', (request, response) => {
 	return listCategoryController.handle(request, response);
+});
+
+categoryRoute.post('/import', upload.single('file'), (request, response) => {
+	const { file } = request;
+
+	console.log(file);
+	return response.send();
 });
 
 export { categoryRoute };
